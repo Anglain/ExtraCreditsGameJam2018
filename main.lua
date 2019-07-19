b04bd44bf1e4739f913ca6bf404f7b6d4d71c4aa
Anglain <shudra.ia@gmail.com>
2019-07-19 11:44:10 +0300

    Font tests, monospaced font added (and a few more)

--- main.lua
+++ main.lua
@@ -8,13 +8,39 @@
 io.stdout:setvbuf('no')
 
 function love.load()
+	fontSize = 30
 
+	fontMonospaced = love.graphics.newFont('fonts/PTMono-Regular.ttf', fontSize)
+
+	letterChargeTime = 0.1
+	pauseTime = 1
+
+	text = 'And so...'
+	text2 = 'It began'
+	drawnText = ''
+	textCoords = {x = 50, y = 70}
+	currentTime = 0
+	currentLetterIndex = 1
+	drawingLetters = true
 end
 
 function love.update(dt)
+	currentTime = currentTime + dt
 
+	if drawingLetters and currentTime >= letterChargeTime then
+
+		currentTime = 0
+
+		charFromIndex = text:sub(currentLetterIndex, currentLetterIndex)
+		drawnText = drawnText .. charFromIndex
+		drawableCharFromIndex = love.graphics.newText(fontMonospaced, charFromIndex)
+
+		currentLetterIndex = currentLetterIndex + 1
+		drawingLetters = not (currentLetterIndex > #text)
+	end
 end
 
 function love.draw()
-
+	love.graphics.setFont(fontMonospaced)
+	love.graphics.draw(love.graphics.newText(fontMonospaced, drawnText), textCoords.x, textCoords.y)
 end
