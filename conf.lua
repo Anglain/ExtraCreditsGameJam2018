143df258ae4bde9c2cddac9c7dad54f480751ea1
Anglain <shudra.ia@gmail.com>
2019-07-19 10:23:13 +0300

    Proj reload

---
+++ conf.lua
@@ -0,0 +1,9 @@
+--[==[
+-- Saya project
+--
+-- Created on 19.07.2019
+--]==]
+
+function love.conf(t)
+
+end
---
+++ main.lua
@@ -0,0 +1,20 @@
+--[==[
+-- Saya project
+--
+-- Created on 19.07.2019
+--]==]
+
+-- Flusihing output to the console immideately
+io.stdout:setvbuf('no')
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
