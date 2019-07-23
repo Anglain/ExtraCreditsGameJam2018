dccc947421111ced315e0e3f3806e24f04bd2eab
Anglain <shudra.ia@gmail.com>
2019-07-23 10:50:10 +0300

    Added grid to the map

--- classes/Map.lua
+++ classes/Map.lua
@@ -17,7 +17,8 @@ function Map:new(tileSize, mapTiles)
 	local map = {
 		tileSize = tileSize,
 		tilesNumber = mapTiles,
-		color = {206/255, 214/255, 245/255, 1}
+		color = {206/255, 214/255, 245/255, 1},
+		color2 = {0, 0, 0, 1}
 	}
 
 	function map:update(dt)
@@ -28,6 +29,12 @@ function Map:new(tileSize, mapTiles)
 		currentColor[1], currentColor[2], currentColor[3], currentColor[4] = love.graphics.getColor()
 		love.graphics.setColor(map.color)
 		love.graphics.rectangle('fill', 0, 0, map.tileSize * map.tilesNumber, map.tileSize * map.tilesNumber)
+		love.graphics.setColor(map.color2)
+		for i = 1,map.tilesNumber do
+			for j = 1, map.tilesNumber do
+				love.graphics.rectangle('line', (i-1) * map.tileSize, (j-1) * map.tileSize, map.tileSize, map.tileSize, 4, 4)
+			end
+		end
 		love.graphics.setColor(currentColor)
 	end
 
